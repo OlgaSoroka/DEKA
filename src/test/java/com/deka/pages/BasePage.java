f42bf51ffd62d14d389b1feebea70e2c95b591b3
olhasoroka <olly0296@gmail.com>
2023-02-16 18:02:03 -0600

    utility, basepage are created

--- src/test/java/com/deka/pages/BasePage.java
+++ src/test/java/com/deka/pages/BasePage.java
@@ -1,4 +1,10 @@
 package com.deka.pages;
+import com.deka.utilities.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
---
+++ src/test/java/com/deka/runners/CukesRunner.java
@@ -0,0 +1,25 @@
+package com.deka.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/deka/step_definitions",
+        features = "src/test/resources/features",
+        tags = "",
+        dryRun = false
+
+)
+
+
+
+
+
+public class CukesRunner {
+
+
+
+
+}
